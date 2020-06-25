21af7b46c9ff70b9526b37b791bbf39ab812f844
David Torralba Goitia <david.torralba.goitia@gmail.com>
2020-06-25 20:13:17 +0100

    reformat tests

--- test_connectz.py
+++ test_connectz.py
@@ -15,36 +15,28 @@ class ConnectzTest(unittest.TestCase):
     @patch('connectz.log')
     def test_no_input_files_provided(self, mocked_log):
         connectz.main()
-        expected_log_calls = (
-            call('Provide one input file'),
-        )
+        expected_log_calls = (call('Provide one input file'),)
         mocked_log.assert_has_calls(expected_log_calls)
 
     @patch.object(sys, 'argv', ['connectz.py', EXISTING_FILE, EXISTING_FILE, NON_EXISTING_FILE])
     @patch('connectz.log')
     def test_many_input_files_provided(self, mocked_log):
         connectz.main()
-        expected_log_calls = (
-            call('Provide one input file'),
-        )
+        expected_log_calls = (call('Provide one input file'),)
         mocked_log.assert_has_calls(expected_log_calls)
 
     @patch.object(sys, 'argv', ['connectz.py', 'impossible_game'])
     @patch('connectz.log')
     def test_impossible_game(self, mocked_log):
         connectz.main()
-        expected_log_calls = (
-            call('7'),
-        )
+        expected_log_calls = (call('7'),)
         mocked_log.assert_has_calls(expected_log_calls)
 
     @patch.object(sys, 'argv', ['connectz.py', 'illegal_game'])
     @patch('connectz.log')
     def test_illegal_game(self, mocked_log):
         connectz.main()
-        expected_log_calls = (
-            call('8'),
-        )
+        expected_log_calls = (call('8'),)
         mocked_log.assert_has_calls(expected_log_calls)
 
     @patch.object(sys, 'argv', ['connectz.py', NON_EXISTING_FILE])
