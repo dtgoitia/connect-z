a2f57ef15a507a0ba3b1cbd7fe9f0e4ead57e8a6
David Torralba Goitia <david.torralba@babylonhealth.com>
2020-07-07 08:56:28 +0100

    Improve profiling flow

--- create_game.py
+++ create_game.py
@@ -3,18 +3,22 @@ import sys
 
 
 ITERATIONS = 5_000  # 4 rows per iteration
+MOVES = '1\n2\n2\n1\n'
+
+def create_game(file_name, iterations = ITERATIONS, moves = MOVES) -> pathlib.Path:
+    path = pathlib.Path.cwd() / 'profilling' / file_name
+    with open(path, 'w') as file:
+        rows = 4 * iterations
+        file.write(f'2 {rows} 3')
+        for _ in range(iterations):
+            file.write(MOVES)
+    print(f'File "{path}" created with {rows:,} rows')
+    return path
 
 
 def main():
-    moves = ('1\n2\n2\n1\n')
     file_name = sys.argv[1]
-    file_path = pathlib.Path.cwd() / 'profilling' / file_name
-    with open(file_path, 'w') as file:
-        rows = 4 * ITERATIONS
-        file.write(f'2 {rows} 3')
-        for _ in range(ITERATIONS):
-            file.write(moves)
-    print(f'File "{file_path}" created with {rows:,} rows')
+    create_game(file_name)
 
 
 if __name__ == "__main__":
