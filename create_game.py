43064ac930130ec9d6a8793452b9ecbb0b4a4846
David Torralba Goitia <david.torralba@babylonhealth.com>
2020-07-14 21:33:17 +0100

    Profile game with 10^6 moves

--- create_game.py
+++ create_game.py
@@ -1,20 +1,53 @@
+import itertools
+import math
 import pathlib
 import sys
 
 
-ITERATIONS = 5_000
+def move_generator(columns: int, rows: int, line_length: int):
+    if line_length < 3:
+        raise ValueError(f'line length must be 3 or bigger')
+
+    # columns: 1 2 3 4     move sequence:
+    # row 1:   A A B B     1 3 2 4
+    # row 2:   B B A A     6 8 5 7
+    basic_block = [1, 3, 2, 4, 3, 1, 4, 2]
+    block_length = 4
+    block_height = 2
+
+    # how many basic blocks fit in the board considering the column amount
+    h_size = math.floor(columns / block_length)
+
+    # how many basic blocks fit in the board considering the row amount
+    v_size = math.floor(rows / block_height)
+
+    def block_generator(start_column: int):
+        relocated_block = (column + start_column for column in basic_block)
+        return relocated_block
+
+    start_columns = range(0, columns - block_length + 1, block_length)
+    start_rows = range(0, rows - block_height + 1, block_height)
+
+    for start_row in start_rows:
+        for start_column in start_columns:
+            for move in block_generator(start_column):
+                yield move
+
+
+def create_game(file_name, *, columns, rows, line_length) -> pathlib.Path:
+    path = pathlib.Path.cwd() / 'profilling' / file_name
+    with open(path, 'w') as file:
+        file.write(f'{columns} {rows} {line_length}\n')
+        for i, move in enumerate(move_generator(columns, rows, line_length)):
+            file.write(f'{move}\n')
+    print(f'File "{path}" created with {i:,} moves')
+    return path
 
 
 def main():
-    moves = "1\n2\n2\n1\n"
     file_name = sys.argv[1]
-    file_path = pathlib.Path.cwd() / "profilling" / file_name
-    with open(file_path, "w") as file:
-        rows = 4 * ITERATIONS
-        file.write(f"2 {rows} 3")
-        for _ in range(ITERATIONS):
-            file.write(moves)
-    print(f'File "{file_path}" created with {rows:,} rows')
+    n = int(sys.argv[2])
+    create_game(file_name, columns=n, rows=n, line_length=n)
 
 
 if __name__ == "__main__":
--- profile_game.py
+++ profile_game.py
@@ -1,39 +1,73 @@
 from cProfile import Profile
 import connectz
 import pathlib
+import shutil
+import statistics
 import subprocess
 import sys
 import time
 
+from create_game import create_game
 
-def main():
-    print("Mocking command arguments...")
-    file_name = sys.argv[1]
-    file_path = pathlib.Path.cwd() / "profilling" / file_name
-    sys.argv[1] = str(file_path)
+
+PROFILLING_DIR = 'profilling'
+FILE_NAME = 'game_for_profilling'
+
+
+def profile_game(file_name: str):
+    print('Mocking command arguments...')
+    file_path = pathlib.Path.cwd() / PROFILLING_DIR / file_name
+    if len(sys.argv) == 1:
+        sys.argv.append(str(file_path))
+    else:
+        sys.argv[1] = str(file_path)
 
     profiler = Profile()
-    print("Collecting stats...")
+    print('Collecting stats...')
     profiler.enable()
-    print("Run connectz.main()")
+    print('Run connectz.main()')
     start = time.time()
     connectz.main()
     end = time.time()
     profiler.disable()
 
     # Dump stats
-    print("Dumping collected stats...", end="")
-    profiler.dump_stats(f"{file_path}.pstats")
-    print(" done")
+    print('Dumping collected stats...', end='')
+    profiler.dump_stats(f'{file_path}.pstats')
+    print(' done')
 
     # Create chart from stats
-    print("Creating profile tree...", end="")
-    ps = subprocess.Popen(["gprof2dot", "-f", "pstats", f"{file_path}.pstats"], stdout=subprocess.PIPE)
-    output = subprocess.check_output(["dot", "-Tpng", "-o", f"{file_path}.png"], stdin=ps.stdout)
+    print('Creating profile tree...', end='')
+    ps = subprocess.Popen(['gprof2dot', '-f', 'pstats', f'{file_path}.pstats'], stdout=subprocess.PIPE)
+    output = subprocess.check_output(['dot', '-Tpng', '-o', f'{file_path}.png'], stdin=ps.stdout)
     ps.wait()
-    print(" done")
-    print(f"Execution time: {end - start:.4f}s")
+    print(' done')
+
+    execution_time = end-start
+    print(f'Execution time: {execution_time:.4f}s')
+    return execution_time
 
 
-if __name__ == "__main__":
+def main():
+    n = 50
+    game_path = create_game(FILE_NAME, columns=n, rows=n, line_length=n)
+    durations = []
+
+    for n in range(4):
+        file_name = f'{FILE_NAME}_{n}'
+        game_copy_path = game_path.parent / file_name
+        shutil.copyfile(game_path, game_copy_path)
+        duration = profile_game(file_name)
+        durations.append(duration)
+
+    durations_file = pathlib.Path.cwd() / PROFILLING_DIR / 'durations'
+    with durations_file.open('a') as f:
+        mean = statistics.mean(durations)
+        print(f'Average execution time: {mean:.4f}s')
+        formatted = [f'{t:.4f}' for t in (mean, *durations)]
+        line = f"{' '.join(formatted)}\n"
+        f.write(line)
+
+
+if __name__ == '__main__':
     main()
